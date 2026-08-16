60fbaf9bad4fb55b5297887889e05d4f6b6607c9
xjemtopi-lang <xjemtopi@gmail.com>
2026-08-16 20:00:11 +0500

    unzipgit

---
+++ meduza/meduza/obj/local/x86_64/objs/magaisanware/__/includes/draw/ImGui/backends/imgui_impl_android.o.d
@@ -0,0 +1,8 @@
+./obj/local/x86_64/objs/magaisanware/__/includes/draw/ImGui/backends/imgui_impl_android.o: \
+  jni/../includes/draw/ImGui/backends/imgui_impl_android.cpp \
+  jni/../includes/internal/ImGui/imgui.h \
+  jni/../includes/internal/ImGui/imconfig.h \
+  jni/../includes/internal/ImGui/backends/imgui_impl_android.h
+jni/../includes/internal/ImGui/imgui.h:
+jni/../includes/internal/ImGui/imconfig.h:
+jni/../includes/internal/ImGui/backends/imgui_impl_android.h:
