cd81157764025173bb2df296d5d1eb475df755f4
xjemtopi-lang <xjemtopi@gmail.com>
2026-08-16 20:03:17 +0500

    unzipgit

---
+++ meduza/meduza/src/func/movement.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace air_jump {
+    void run();
+}
+
+namespace strafe {
+    void run();
+}
+
+namespace bunny_hop {
+    void run();
+}
+
+namespace crouch_speed {
+    void run();
+}
