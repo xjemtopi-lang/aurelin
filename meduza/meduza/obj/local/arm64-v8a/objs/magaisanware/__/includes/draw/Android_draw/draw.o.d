5b034571ea30f7aa9a1f69461122ce31e2995ce2
xjemtopi-lang <xjemtopi@gmail.com>
2026-08-16 19:57:45 +0500

    unzipgit

---
+++ meduza/meduza/obj/local/arm64-v8a/objs/magaisanware/__/includes/draw/Android_draw/draw.o.d
@@ -0,0 +1,21 @@
+./obj/local/arm64-v8a/objs/magaisanware/__/includes/draw/Android_draw/draw.o: \
+  jni/../includes/draw/Android_draw/draw.cpp \
+  jni/../includes/internal/Android_draw/draw.h \
+  jni/../includes/internal/native_surface/ANativeWindowCreator.h \
+  jni/../includes/internal/ImGui/imgui.h \
+  jni/../includes/internal/ImGui/imconfig.h \
+  jni/../includes/internal/ImGui/imgui_internal.h \
+  jni/../includes/internal/ImGui/backends/imgui_impl_opengl3.h \
+  jni/../includes/internal/ImGui/backends/imgui_impl_android.h \
+  jni/../includes/internal/Android_touch/Touch.hpp \
+  jni/../includes/fonts/menu/comfortaa.h jni/../includes/fonts/esp/esp.h
+jni/../includes/internal/Android_draw/draw.h:
+jni/../includes/internal/native_surface/ANativeWindowCreator.h:
+jni/../includes/internal/ImGui/imgui.h:
+jni/../includes/internal/ImGui/imconfig.h:
+jni/../includes/internal/ImGui/imgui_internal.h:
+jni/../includes/internal/ImGui/backends/imgui_impl_opengl3.h:
+jni/../includes/internal/ImGui/backends/imgui_impl_android.h:
+jni/../includes/internal/Android_touch/Touch.hpp:
+jni/../includes/fonts/menu/comfortaa.h:
+jni/../includes/fonts/esp/esp.h:
