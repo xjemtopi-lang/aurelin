a3cdaa9c10014de0a6beb0bd39eb2d79cae0a358
xjemtopi-lang <xjemtopi@gmail.com>
2026-08-16 19:57:54 +0500

    unzipgit

---
+++ meduza/meduza/obj/local/arm64-v8a/objs/magaisanware/__/includes/draw/ImGui/backends/imgui_impl_opengl3.o.d
@@ -0,0 +1,8 @@
+./obj/local/arm64-v8a/objs/magaisanware/__/includes/draw/ImGui/backends/imgui_impl_opengl3.o: \
+  jni/../includes/draw/ImGui/backends/imgui_impl_opengl3.cpp \
+  jni/../includes/internal/ImGui/imgui.h \
+  jni/../includes/internal/ImGui/imconfig.h \
+  jni/../includes/internal/ImGui/backends/imgui_impl_opengl3.h
+jni/../includes/internal/ImGui/imgui.h:
+jni/../includes/internal/ImGui/imconfig.h:
+jni/../includes/internal/ImGui/backends/imgui_impl_opengl3.h:
