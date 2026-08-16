887a9431185f0358a08464b8e0c3753e812b9a83
xjemtopi-lang <xjemtopi@gmail.com>
2026-08-16 20:02:37 +0500

    unzipgit

---
+++ meduza/meduza/obj/local/x86_64/objs/magaisanware/__/src/ui/cfg_system.o.d
@@ -0,0 +1,14 @@
+./obj/local/x86_64/objs/magaisanware/__/src/ui/cfg_system.o: \
+  jni/../src/ui/cfg_system.cpp jni/../src/ui/cfg_system.hpp \
+  jni/../src/ui/cfg.hpp jni/../includes/internal/ImGui/imgui.h \
+  jni/../includes/internal/ImGui/imconfig.h jni/../src/ui/cfg_holy.hpp \
+  jni/../src/ui/../func/combat.hpp jni/../src/ui/../func/props.hpp \
+  jni/../src/ui/../func/gfx.hpp
+jni/../src/ui/cfg_system.hpp:
+jni/../src/ui/cfg.hpp:
+jni/../includes/internal/ImGui/imgui.h:
+jni/../includes/internal/ImGui/imconfig.h:
+jni/../src/ui/cfg_holy.hpp:
+jni/../src/ui/../func/combat.hpp:
+jni/../src/ui/../func/props.hpp:
+jni/../src/ui/../func/gfx.hpp:
