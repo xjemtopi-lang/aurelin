94f88ba8001922c327cc53577c38a01778b1bc7b
xjemtopi-lang <xjemtopi@gmail.com>
2026-08-16 20:01:18 +0500

    unzipgit

---
+++ meduza/meduza/obj/local/x86_64/objs/magaisanware/__/src/func/inf_shop.o.d
@@ -0,0 +1,23 @@
+./obj/local/x86_64/objs/magaisanware/__/src/func/inf_shop.o: \
+  jni/../src/func/inf_shop.cpp jni/../src/func/inf_shop.hpp \
+  jni/../src/func/../game/game.hpp \
+  jni/../src/func/../game/../other/memory.hpp \
+  jni/../src/func/../game/../other/../protect/oxorany.hpp \
+  jni/../src/func/../game/../protect/oxorany.hpp \
+  jni/../src/func/../game/offsets.hpp \
+  jni/../src/func/../ui/cfg_holy_bridge.hpp \
+  jni/../src/func/../ui/cfg_holy.hpp \
+  jni/../includes/internal/ImGui/imgui.h \
+  jni/../includes/internal/ImGui/imconfig.h \
+  jni/../src/func/../protect/oxorany.hpp
+jni/../src/func/inf_shop.hpp:
+jni/../src/func/../game/game.hpp:
+jni/../src/func/../game/../other/memory.hpp:
+jni/../src/func/../game/../other/../protect/oxorany.hpp:
+jni/../src/func/../game/../protect/oxorany.hpp:
+jni/../src/func/../game/offsets.hpp:
+jni/../src/func/../ui/cfg_holy_bridge.hpp:
+jni/../src/func/../ui/cfg_holy.hpp:
+jni/../includes/internal/ImGui/imgui.h:
+jni/../includes/internal/ImGui/imconfig.h:
+jni/../src/func/../protect/oxorany.hpp:
