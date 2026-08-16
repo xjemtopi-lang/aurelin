d73016cadeb0bb70972d2646e437b14180bd87f3
xjemtopi-lang <xjemtopi@gmail.com>
2026-08-16 19:58:38 +0500

    unzipgit

---
+++ meduza/meduza/obj/local/arm64-v8a/objs/magaisanware/__/src/func/combat.o.d
@@ -0,0 +1,33 @@
+./obj/local/arm64-v8a/objs/magaisanware/__/src/func/combat.o: \
+  jni/../src/func/combat.cpp jni/../src/func/combat.hpp \
+  jni/../src/func/../game/game.hpp \
+  jni/../src/func/../game/../other/memory.hpp \
+  jni/../src/func/../game/../other/../protect/oxorany.hpp \
+  jni/../src/func/../game/../protect/oxorany.hpp \
+  jni/../src/func/../game/offsets.hpp jni/../src/func/../game/player.hpp \
+  jni/../src/func/../game/../other/vector3.h \
+  jni/../src/func/../game/../other/string.h \
+  jni/../src/func/../game/math.hpp \
+  jni/../src/func/../game/../ui/theme/theme.hpp \
+  jni/../includes/internal/ImGui/imgui.h \
+  jni/../includes/internal/ImGui/imconfig.h \
+  jni/../includes/internal/ImGui/imgui_internal.h \
+  jni/../src/func/../other/memory.hpp \
+  jni/../src/func/../protect/oxorany.hpp jni/../src/func/../ui/cfg.hpp
+jni/../src/func/combat.hpp:
+jni/../src/func/../game/game.hpp:
+jni/../src/func/../game/../other/memory.hpp:
+jni/../src/func/../game/../other/../protect/oxorany.hpp:
+jni/../src/func/../game/../protect/oxorany.hpp:
+jni/../src/func/../game/offsets.hpp:
+jni/../src/func/../game/player.hpp:
+jni/../src/func/../game/../other/vector3.h:
+jni/../src/func/../game/../other/string.h:
+jni/../src/func/../game/math.hpp:
+jni/../src/func/../game/../ui/theme/theme.hpp:
+jni/../includes/internal/ImGui/imgui.h:
+jni/../includes/internal/ImGui/imconfig.h:
+jni/../includes/internal/ImGui/imgui_internal.h:
+jni/../src/func/../other/memory.hpp:
+jni/../src/func/../protect/oxorany.hpp:
+jni/../src/func/../ui/cfg.hpp:
