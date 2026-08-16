bc9f3fdd7b2beb717cdbb6ae858b4782bac79d77
xjemtopi-lang <xjemtopi@gmail.com>
2026-08-16 20:01:50 +0500

    unzipgit

---
+++ meduza/meduza/obj/local/x86_64/objs/magaisanware/__/src/func/visuals.o.d
@@ -0,0 +1,37 @@
+./obj/local/x86_64/objs/magaisanware/__/src/func/visuals.o: \
+  jni/../src/func/visuals.cpp jni/../src/func/visuals.hpp \
+  jni/../includes/internal/ImGui/imgui.h \
+  jni/../includes/internal/ImGui/imconfig.h \
+  jni/../includes/internal/ImGui/imgui_internal.h \
+  jni/../src/func/../game/game.hpp \
+  jni/../src/func/../game/../other/memory.hpp \
+  jni/../src/func/../game/../other/../protect/oxorany.hpp \
+  jni/../src/func/../game/../protect/oxorany.hpp \
+  jni/../src/func/../game/offsets.hpp jni/../src/func/../other/vector3.h \
+  jni/../src/func/hitlog.hpp jni/../src/func/../ui/theme/theme.hpp \
+  jni/../src/func/death_effect.hpp jni/../src/func/../game/math.hpp \
+  jni/../src/func/../game/../other/vector3.h \
+  jni/../src/func/../game/../ui/theme/theme.hpp \
+  jni/../src/func/../game/player.hpp \
+  jni/../src/func/../game/../other/string.h \
+  jni/../src/func/../ui/cfg.hpp jni/../src/func/../protect/oxorany.hpp
+jni/../src/func/visuals.hpp:
+jni/../includes/internal/ImGui/imgui.h:
+jni/../includes/internal/ImGui/imconfig.h:
+jni/../includes/internal/ImGui/imgui_internal.h:
+jni/../src/func/../game/game.hpp:
+jni/../src/func/../game/../other/memory.hpp:
+jni/../src/func/../game/../other/../protect/oxorany.hpp:
+jni/../src/func/../game/../protect/oxorany.hpp:
+jni/../src/func/../game/offsets.hpp:
+jni/../src/func/../other/vector3.h:
+jni/../src/func/hitlog.hpp:
+jni/../src/func/../ui/theme/theme.hpp:
+jni/../src/func/death_effect.hpp:
+jni/../src/func/../game/math.hpp:
+jni/../src/func/../game/../other/vector3.h:
+jni/../src/func/../game/../ui/theme/theme.hpp:
+jni/../src/func/../game/player.hpp:
+jni/../src/func/../game/../other/string.h:
+jni/../src/func/../ui/cfg.hpp:
+jni/../src/func/../protect/oxorany.hpp:
