d10b00245f5bc8cd8c60dc3f469dcb2b391c2d9e
xjemtopi-lang <xjemtopi@gmail.com>
2026-08-16 20:01:23 +0500

    unzipgit

---
+++ meduza/meduza/obj/local/x86_64/objs/magaisanware/__/src/func/movement.o.d
@@ -0,0 +1,27 @@
+./obj/local/x86_64/objs/magaisanware/__/src/func/movement.o: \
+  jni/../src/func/movement.cpp jni/../src/func/movement.hpp \
+  jni/../src/func/../game/game.hpp \
+  jni/../src/func/../game/../other/memory.hpp \
+  jni/../src/func/../game/../other/../protect/oxorany.hpp \
+  jni/../src/func/../game/../protect/oxorany.hpp \
+  jni/../src/func/../game/offsets.hpp jni/../src/func/../game/player.hpp \
+  jni/../src/func/../game/../other/vector3.h \
+  jni/../src/func/../game/../other/string.h \
+  jni/../src/func/../other/memory.hpp jni/../src/func/../ui/cfg_holy.hpp \
+  jni/../includes/internal/ImGui/imgui.h \
+  jni/../includes/internal/ImGui/imconfig.h \
+  jni/../src/func/../protect/oxorany.hpp
+jni/../src/func/movement.hpp:
+jni/../src/func/../game/game.hpp:
+jni/../src/func/../game/../other/memory.hpp:
+jni/../src/func/../game/../other/../protect/oxorany.hpp:
+jni/../src/func/../game/../protect/oxorany.hpp:
+jni/../src/func/../game/offsets.hpp:
+jni/../src/func/../game/player.hpp:
+jni/../src/func/../game/../other/vector3.h:
+jni/../src/func/../game/../other/string.h:
+jni/../src/func/../other/memory.hpp:
+jni/../src/func/../ui/cfg_holy.hpp:
+jni/../includes/internal/ImGui/imgui.h:
+jni/../includes/internal/ImGui/imconfig.h:
+jni/../src/func/../protect/oxorany.hpp:
