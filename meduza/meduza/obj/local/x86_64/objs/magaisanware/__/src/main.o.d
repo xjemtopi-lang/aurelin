d37f186700c94011c647c2a4d62540802edc6927
xjemtopi-lang <xjemtopi@gmail.com>
2026-08-16 20:02:28 +0500

    unzipgit

---
+++ meduza/meduza/obj/local/x86_64/objs/magaisanware/__/src/main.o.d
@@ -0,0 +1,60 @@
+./obj/local/x86_64/objs/magaisanware/__/src/main.o: jni/../src/main.cpp \
+  jni/../includes/internal/Android_draw/draw.h \
+  jni/../includes/internal/native_surface/ANativeWindowCreator.h \
+  jni/../includes/internal/ImGui/imgui.h \
+  jni/../includes/internal/ImGui/imconfig.h \
+  jni/../includes/internal/ImGui/imgui_internal.h \
+  jni/../includes/internal/ImGui/backends/imgui_impl_opengl3.h \
+  jni/../includes/internal/ImGui/backends/imgui_impl_android.h \
+  jni/../includes/internal/Android_touch/Touch.hpp \
+  jni/../src/ui/theme/theme.hpp jni/../src/ui/menu.hpp \
+  jni/../src/ui/bar.hpp jni/../src/other/memory.hpp \
+  jni/../src/other/../protect/oxorany.hpp jni/../src/game/game.hpp \
+  jni/../src/game/../other/memory.hpp \
+  jni/../src/game/../protect/oxorany.hpp jni/../src/game/offsets.hpp \
+  jni/../src/func/visuals.hpp jni/../src/func/../game/game.hpp \
+  jni/../src/func/../other/vector3.h jni/../src/func/combat.hpp \
+  jni/../src/func/wallshot.hpp jni/../src/func/inf_ammo.hpp \
+  jni/../src/func/inf_shop.hpp jni/../src/func/fustknife.hpp \
+  jni/../src/func/norecoil.hpp jni/../src/func/fire_rate.hpp \
+  jni/../src/func/anti_effects.hpp jni/../src/func/chams.hpp \
+  jni/../src/func/test_loader.hpp jni/../src/func/movement.hpp \
+  jni/../src/func/fov_changer.hpp jni/../src/func/sigma.hpp \
+  jni/../src/func/props.hpp jni/../src/ui/cfg_holy.hpp \
+  jni/../src/protect/oxorany.hpp
+jni/../includes/internal/Android_draw/draw.h:
+jni/../includes/internal/native_surface/ANativeWindowCreator.h:
+jni/../includes/internal/ImGui/imgui.h:
+jni/../includes/internal/ImGui/imconfig.h:
+jni/../includes/internal/ImGui/imgui_internal.h:
+jni/../includes/internal/ImGui/backends/imgui_impl_opengl3.h:
+jni/../includes/internal/ImGui/backends/imgui_impl_android.h:
+jni/../includes/internal/Android_touch/Touch.hpp:
+jni/../src/ui/theme/theme.hpp:
+jni/../src/ui/menu.hpp:
+jni/../src/ui/bar.hpp:
+jni/../src/other/memory.hpp:
+jni/../src/other/../protect/oxorany.hpp:
+jni/../src/game/game.hpp:
+jni/../src/game/../other/memory.hpp:
+jni/../src/game/../protect/oxorany.hpp:
+jni/../src/game/offsets.hpp:
+jni/../src/func/visuals.hpp:
+jni/../src/func/../game/game.hpp:
+jni/../src/func/../other/vector3.h:
+jni/../src/func/combat.hpp:
+jni/../src/func/wallshot.hpp:
+jni/../src/func/inf_ammo.hpp:
+jni/../src/func/inf_shop.hpp:
+jni/../src/func/fustknife.hpp:
+jni/../src/func/norecoil.hpp:
+jni/../src/func/fire_rate.hpp:
+jni/../src/func/anti_effects.hpp:
+jni/../src/func/chams.hpp:
+jni/../src/func/test_loader.hpp:
+jni/../src/func/movement.hpp:
+jni/../src/func/fov_changer.hpp:
+jni/../src/func/sigma.hpp:
+jni/../src/func/props.hpp:
+jni/../src/ui/cfg_holy.hpp:
+jni/../src/protect/oxorany.hpp:
