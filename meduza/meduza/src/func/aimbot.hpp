86ca3470940b56e0ebbef9a784b9db39b18d5c19
xjemtopi-lang <xjemtopi@gmail.com>
2026-08-16 20:02:49 +0500

    unzipgit

---
+++ meduza/meduza/src/func/aimbot.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <cstdint>
+#include "../game/math.hpp"
+#include "../other/vector3.h"
+#include "imgui.h"
+
+namespace aimbot {
+    void handle();
+    void draw_fov_circle();
+}
