dda51990314b3c4a473a7155610e437ce7cf891e
xjemtopi-lang <xjemtopi@gmail.com>
2026-08-16 20:00:20 +0500

    unzipgit

---
+++ meduza/meduza/obj/local/x86_64/objs/magaisanware/__/includes/draw/ImGui/imgui.o.d
@@ -0,0 +1,8 @@
+./obj/local/x86_64/objs/magaisanware/__/includes/draw/ImGui/imgui.o: \
+  jni/../includes/draw/ImGui/imgui.cpp \
+  jni/../includes/draw/ImGui/imgui.h \
+  jni/../includes/draw/ImGui/imconfig.h \
+  jni/../includes/draw/ImGui/imgui_internal.h
+jni/../includes/draw/ImGui/imgui.h:
+jni/../includes/draw/ImGui/imconfig.h:
+jni/../includes/draw/ImGui/imgui_internal.h:
