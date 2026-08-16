0c13c2d5aaef1a776cf4d7ca580aeb1a43d092cb
xjemtopi-lang <xjemtopi@gmail.com>
2026-08-16 20:02:34 +0500

    unzipgit

---
+++ meduza/meduza/obj/local/x86_64/objs/magaisanware/__/src/ui/bar.o.d
@@ -0,0 +1,10 @@
+./obj/local/x86_64/objs/magaisanware/__/src/ui/bar.o: \
+  jni/../src/ui/bar.cpp jni/../src/ui/bar.hpp \
+  jni/../src/ui/theme/theme.hpp jni/../includes/internal/ImGui/imgui.h \
+  jni/../includes/internal/ImGui/imconfig.h \
+  jni/../includes/internal/ImGui/imgui_internal.h
+jni/../src/ui/bar.hpp:
+jni/../src/ui/theme/theme.hpp:
+jni/../includes/internal/ImGui/imgui.h:
+jni/../includes/internal/ImGui/imconfig.h:
+jni/../includes/internal/ImGui/imgui_internal.h:
