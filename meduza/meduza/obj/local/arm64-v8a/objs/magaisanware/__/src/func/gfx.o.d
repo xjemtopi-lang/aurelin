701c747ffe8e8faf4cafc2b52723c2c1848259dc
xjemtopi-lang <xjemtopi@gmail.com>
2026-08-16 19:58:51 +0500

    unzipgit

---
+++ meduza/meduza/obj/local/arm64-v8a/objs/magaisanware/__/src/func/gfx.o.d
@@ -0,0 +1,7 @@
+./obj/local/arm64-v8a/objs/magaisanware/__/src/func/gfx.o: \
+  jni/../src/func/gfx.cpp jni/../src/func/gfx.hpp \
+  jni/../src/func/../other/memory.hpp \
+  jni/../src/func/../other/../protect/oxorany.hpp
+jni/../src/func/gfx.hpp:
+jni/../src/func/../other/memory.hpp:
+jni/../src/func/../other/../protect/oxorany.hpp:
