13aa50a7f15fb43541d8ce65308c16178abcf48c
xjemtopi-lang <xjemtopi@gmail.com>
2026-08-16 19:58:31 +0500

    unzipgit

---
+++ meduza/meduza/obj/local/arm64-v8a/objs/magaisanware/__/src/func/chams.o.d
@@ -0,0 +1,13 @@
+./obj/local/arm64-v8a/objs/magaisanware/__/src/func/chams.o: \
+  jni/../src/func/chams.cpp jni/../src/func/chams.hpp \
+  jni/../src/func/../ui/cfg_holy_bridge.hpp \
+  jni/../src/func/../ui/cfg_holy.hpp \
+  jni/../includes/internal/ImGui/imgui.h \
+  jni/../includes/internal/ImGui/imconfig.h \
+  jni/../src/func/../protect/oxorany.hpp
+jni/../src/func/chams.hpp:
+jni/../src/func/../ui/cfg_holy_bridge.hpp:
+jni/../src/func/../ui/cfg_holy.hpp:
+jni/../includes/internal/ImGui/imgui.h:
+jni/../includes/internal/ImGui/imconfig.h:
+jni/../src/func/../protect/oxorany.hpp:
