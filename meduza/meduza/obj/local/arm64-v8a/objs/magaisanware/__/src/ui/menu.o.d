c8916cc52e48dfb7fd20031ab2b07574385f76cc
xjemtopi-lang <xjemtopi@gmail.com>
2026-08-16 19:59:48 +0500

    unzipgit

---
+++ meduza/meduza/obj/local/arm64-v8a/objs/magaisanware/__/src/ui/menu.o.d
@@ -0,0 +1,21 @@
+./obj/local/arm64-v8a/objs/magaisanware/__/src/ui/menu.o: \
+  jni/../src/ui/menu.cpp jni/../src/ui/menu.hpp jni/../src/ui/bar.hpp \
+  jni/../src/ui/cfg.hpp jni/../includes/internal/ImGui/imgui.h \
+  jni/../includes/internal/ImGui/imconfig.h \
+  jni/../src/ui/theme/theme.hpp \
+  jni/../includes/internal/ImGui/imgui_internal.h \
+  jni/../src/ui/../func/combat.hpp jni/../src/ui/../func/gfx.hpp \
+  jni/../src/ui/cfg_holy.hpp jni/../src/ui/cfg_holy_bridge.hpp \
+  jni/../src/ui/../func/props.hpp
+jni/../src/ui/menu.hpp:
+jni/../src/ui/bar.hpp:
+jni/../src/ui/cfg.hpp:
+jni/../includes/internal/ImGui/imgui.h:
+jni/../includes/internal/ImGui/imconfig.h:
+jni/../src/ui/theme/theme.hpp:
+jni/../includes/internal/ImGui/imgui_internal.h:
+jni/../src/ui/../func/combat.hpp:
+jni/../src/ui/../func/gfx.hpp:
+jni/../src/ui/cfg_holy.hpp:
+jni/../src/ui/cfg_holy_bridge.hpp:
+jni/../src/ui/../func/props.hpp:
