68027f100ec43efc88fb472154f06e81dfc0af20
xjemtopi-lang <xjemtopi@gmail.com>
2026-08-16 20:01:34 +0500

    unzipgit

---
+++ meduza/meduza/obj/local/x86_64/objs/magaisanware/__/src/func/props.o.d
@@ -0,0 +1,22 @@
+./obj/local/x86_64/objs/magaisanware/__/src/func/props.o: \
+  jni/../src/func/props.cpp jni/../src/func/props.hpp \
+  jni/../src/func/../game/player.hpp jni/../src/func/../game/game.hpp \
+  jni/../src/func/../game/../other/memory.hpp \
+  jni/../src/func/../game/../other/../protect/oxorany.hpp \
+  jni/../src/func/../game/../protect/oxorany.hpp \
+  jni/../src/func/../game/offsets.hpp \
+  jni/../src/func/../game/../other/vector3.h \
+  jni/../src/func/../game/../other/string.h \
+  jni/../src/func/../other/memory.hpp \
+  jni/../src/func/../protect/oxorany.hpp
+jni/../src/func/props.hpp:
+jni/../src/func/../game/player.hpp:
+jni/../src/func/../game/game.hpp:
+jni/../src/func/../game/../other/memory.hpp:
+jni/../src/func/../game/../other/../protect/oxorany.hpp:
+jni/../src/func/../game/../protect/oxorany.hpp:
+jni/../src/func/../game/offsets.hpp:
+jni/../src/func/../game/../other/vector3.h:
+jni/../src/func/../game/../other/string.h:
+jni/../src/func/../other/memory.hpp:
+jni/../src/func/../protect/oxorany.hpp:
