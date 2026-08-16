9026ce824228089b054e8fabd906d395118e8823
xjemtopi-lang <xjemtopi@gmail.com>
2026-08-16 19:57:49 +0500

    unzipgit

---
+++ meduza/meduza/obj/local/arm64-v8a/objs/magaisanware/__/includes/draw/Android_touch/Touch.o.d
@@ -0,0 +1,8 @@
+./obj/local/arm64-v8a/objs/magaisanware/__/includes/draw/Android_touch/Touch.o: \
+  jni/../includes/draw/Android_touch/Touch.cpp \
+  jni/../includes/internal/ImGui/imgui.h \
+  jni/../includes/internal/ImGui/imconfig.h \
+  jni/../includes/internal/Android_touch/Touch.hpp
+jni/../includes/internal/ImGui/imgui.h:
+jni/../includes/internal/ImGui/imconfig.h:
+jni/../includes/internal/Android_touch/Touch.hpp:
