4ccba81d6eae14e8b0b1441ef16384ddb730c85d
xjemtopi-lang <xjemtopi@gmail.com>
2026-08-16 19:58:45 +0500

    unzipgit

---
+++ meduza/meduza/obj/local/arm64-v8a/objs/magaisanware/__/src/func/fov_changer.o.d
@@ -0,0 +1,22 @@
+./obj/local/arm64-v8a/objs/magaisanware/__/src/func/fov_changer.o: \
+  jni/../src/func/fov_changer.cpp jni/../src/func/fov_changer.hpp \
+  jni/../src/func/../game/game.hpp \
+  jni/../src/func/../game/../other/memory.hpp \
+  jni/../src/func/../game/../other/../protect/oxorany.hpp \
+  jni/../src/func/../game/../protect/oxorany.hpp \
+  jni/../src/func/../game/offsets.hpp \
+  jni/../src/func/../other/memory.hpp jni/../src/func/../ui/cfg_holy.hpp \
+  jni/../includes/internal/ImGui/imgui.h \
+  jni/../includes/internal/ImGui/imconfig.h \
+  jni/../src/func/../protect/oxorany.hpp
+jni/../src/func/fov_changer.hpp:
+jni/../src/func/../game/game.hpp:
+jni/../src/func/../game/../other/memory.hpp:
+jni/../src/func/../game/../other/../protect/oxorany.hpp:
+jni/../src/func/../game/../protect/oxorany.hpp:
+jni/../src/func/../game/offsets.hpp:
+jni/../src/func/../other/memory.hpp:
+jni/../src/func/../ui/cfg_holy.hpp:
+jni/../includes/internal/ImGui/imgui.h:
+jni/../includes/internal/ImGui/imconfig.h:
+jni/../src/func/../protect/oxorany.hpp:
